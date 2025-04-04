83fed04793f79fdd0d89756351a267bf0e517a5e
Denis <korbakovd@gmail.com>
2025-04-04 13:35:27 +0300

    update logic for metrics

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
+use Prometheus\Storage\APC;
 
 Route::prefix('api')->group(base_path('routes/api.php'));
+
+Route::get('/metrics', static function () {
+    $registry = new CollectorRegistry(new APC());
+    $renderer = new RenderTextFormat();
+    $result = $renderer->render($registry->getMetricFamilySamples());
+
+    return response($result, 200)
+        ->header('Content-Type', RenderTextFormat::MIME_TYPE);
+});
